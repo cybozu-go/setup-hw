89f96d47fc12b5470fc9970060f50b4c7dd77deb
dulltz <isrgnoe@gmail.com>
2019-05-30 19:05:20 +0900

    Add metrics

--- redfish/collector.go
+++ redfish/collector.go
@@ -5,6 +5,7 @@ import (
 	"sync/atomic"
 	"time"
 
+	"github.com/cybozu-go/log"
 	"github.com/cybozu-go/setup-hw/gabs"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -20,14 +21,19 @@ type dataMap map[string]*gabs.Container
 
 // Collector implements prometheus.Collector interface.
 type Collector struct {
-	rule    *CollectRule
-	client  Client
-	dataMap atomic.Value
+	rule                          *CollectRule
+	client                        Client
+	dataMap                       atomic.Value
+	lastUpdate                    time.Time
+	lastUpdateDesc                *prometheus.Desc
+	lastUpdateDurationMinutesDesc *prometheus.Desc
 }
 
 // NewCollector returns a new instance of Collector.
 func NewCollector(rule *CollectRule, client Client) (*Collector, error) {
-	return &Collector{rule: rule, client: client}, nil
+	desc1 := prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "last_update"), "", nil, nil)
+	desc2 := prometheus.NewDesc(prometheus.BuildFQName(namespace, "", "last_update_duration_minutes"), "", nil, nil)
+	return &Collector{rule: rule, client: client, lastUpdateDesc: desc1, lastUpdateDurationMinutesDesc: desc2}, nil
 }
 
 // Describe sends descriptions of metrics.
@@ -37,6 +43,8 @@ func (c Collector) Describe(ch chan<- *prometheus.Desc) {
 			ch <- propertyRule.desc
 		}
 	}
+	ch <- c.lastUpdateDesc
+	ch <- c.lastUpdateDurationMinutesDesc
 }
 
 // Collect sends metrics collected from BMC via Redfish.
@@ -53,6 +61,16 @@ func (c Collector) Collect(ch chan<- prometheus.Metric) {
 			ch <- m
 		}
 	}
+	m, err := prometheus.NewConstMetric(c.lastUpdateDesc, prometheus.CounterValue, float64(c.lastUpdate.Unix()))
+	if err != nil {
+		log.Error("failed to register last_update", map[string]interface{}{log.FnError: err})
+	}
+	ch <- m
+	m, err = prometheus.NewConstMetric(c.lastUpdateDurationMinutesDesc, prometheus.GaugeValue, time.Now().Sub(c.lastUpdate).Minutes())
+	if err != nil {
+		log.Error("failed to register last_update_duration_minutes", map[string]interface{}{log.FnError: err})
+	}
+	ch <- m
 }
 
 // Update collects metrics from BMCs via Redfish.
@@ -61,4 +79,5 @@ func (c *Collector) Update(ctx context.Context) {
 	defer cancel()
 	dataMap := c.client.traverse(ctx1)
 	c.dataMap.Store(dataMap)
+	c.lastUpdate = time.Now()
 }
