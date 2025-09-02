9a3be7b894b38e794abeafe79542ce133a5f957f
Maho Takara <takara9@gmail.com>
2025-09-02 00:02:48 +0000

    Add timeout
    Signed-off-by: Maho Takara <takara9@gmail.com>

--- pkg/setup-isoreboot/main.go
+++ pkg/setup-isoreboot/main.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"time"
 
 	"github.com/cybozu-go/log"
 	"github.com/cybozu-go/setup-hw/lib"
@@ -13,7 +14,8 @@ import (
 
 func main() {
 	well.LogConfig{}.Apply()
-	ctx := context.Background()
+	ctx, cancel := context.WithTimeout(context.Background(), 60*time.Second)
+	defer cancel()
 
 	if len(os.Args) < 2 {
 		log.ErrorExit(fmt.Errorf("specify iso image file"))
